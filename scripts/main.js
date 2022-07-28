43172735f0f20e072c8cdb4cfbfb02f909567129
Fabrizio Signoretta <signorettafabrizio@gmail.com>
2022-07-27 22:28:57 -0300

    add data

--- scripts/main.js
+++ scripts/main.js
@@ -29,9 +29,38 @@ function renderCharts (charts) {
   document.querySelector('#charts').innerHTML = html
 }
 
-fetch('../data.json')
-  .then(res => res.json())
-  .then(data => {
-    const sizes = calculate(data)
-    renderCharts(sizes)
-  })
+function fetchData () {
+  const data = [
+    {
+      day: 'mon',
+      amount: 17.45
+    },
+    {
+      day: 'tue',
+      amount: 34.91
+    },
+    {
+      day: 'wed',
+      amount: 52.36
+    },
+    {
+      day: 'thu',
+      amount: 31.07
+    },
+    {
+      day: 'fri',
+      amount: 23.39
+    },
+    {
+      day: 'sat',
+      amount: 43.28
+    },
+    {
+      day: 'sun',
+      amount: 25.48
+    }
+  ]
+  const sizes = calculate(data)
+  renderCharts(sizes)
+}
+fetchData()
